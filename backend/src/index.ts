076403d707a8d7d9e0bbe6b840df6b4b82c6e594
Nicolas Chambon <nicolas.chambon.dev@gmail.com>
2025-03-19 17:35:30 +0100

    add controllers connections to db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,65 +1,103 @@
 import express from "express";
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database("../good_corner.sqlite");
 
 const app = express();
-
-app.use(express.json());
-
 const port = 3000;
 
-let ads = [
-  {
-    id: 1,
-    title: "Bike to sell",
-    description:
-      "My bike is blue, working fine. I'm selling it because I've got a new one",
-    owner: "bike.seller@gmail.com",
-    price: 100,
-    picture:
-      "https://images.lecho.be/view?iid=dc:113129565&context=ONLINE&ratio=16/9&width=640&u=1508242455000",
-    location: "Paris",
-    createdAt: "2023-09-05T10:13:14.755Z",
-  },
-  {
-    id: 2,
-    title: "Car to sell",
-    description:
-      "My car is blue, working fine. I'm selling it because I've got a new one",
-    owner: "car.seller@gmail.com",
-    price: 10000,
-    picture:
-      "https://www.automobile-magazine.fr/asset/cms/34973/config/28294/apres-plusieurs-prototypes-la-bollore-bluecar-a-fini-par-devoiler-sa-version-definitive.jpg",
-    location: "Paris",
-    createdAt: "2023-10-05T10:14:15.922Z",
-  },
-];
+app.use(express.json());
 
 app.get("/", (_req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/ads", (_req, res) => {
-  res.json(ads);
+  db.all("SELECT * FROM AD", (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(err);
+    } else {
+      res.send(rows);
+    }
+  });
 });
 
 app.post("/ads", (req, res) => {
-  ads.push(req.body);
-  res.send("Ad created");
+  const stmt = db.prepare(
+    `INSERT INTO AD (
+      TITLE, 
+      DESCRIPTION, 
+      AUTHOR, 
+      PRICE, 
+      PICTURE_URL, 
+      CITY
+      ) 
+      VALUES (?, ?, ?, ?, ?, ?)`
+  );
+  stmt.run(
+    [
+      req.body.title,
+      req.body.description,
+      req.body.author,
+      req.body.price,
+      req.body.picture_url,
+      req.body.city,
+    ],
+    function (err) {
+      if (err) {
+        console.error(err);
+        res.status(500).send(err);
+      } else {
+        res.status(202).send("Created with success");
+      }
+    }
+  );
 });
 
 app.put("/ads/:id", (req, res) => {
-  ads = ads.map((ad) => {
-    if (ad.id === parseInt(req.params.id)) {
-      return req.body;
-    } else {
-      return ad;
+  const stmt = db.prepare(
+    `UPDATE AD SET 
+      TITLE = ?, 
+      DESCRIPTION = ?, 
+      AUTHOR = ?, 
+      PRICE = ?, 
+      PICTURE_URL = ?, 
+      CITY = ? 
+    WHERE ID = ?`
+  );
+  stmt.run(
+    [
+      req.body.title,
+      req.body.description,
+      req.body.author,
+      req.body.price,
+      req.body.picture_url,
+      req.body.city,
+      req.params.id,
+    ],
+    (err) => {
+      if (err) {
+        console.error(err);
+        res.status;
+      }
     }
-  });
-  res.send("Updated with success");
+  );
 });
 
 app.delete("/ads/:id", (req, res) => {
-  ads = ads.filter((ad) => ad.id !== parseInt(req.params.id));
-  res.send("Deleted with success");
+  db.run("DELETE FROM AD WHERE ID = ?", [req.params.id], (err) => {
+    if (err) {
+      console.error(err);
+      res
+        .status(500)
+        .send(
+          `An error occured while deleting the ad with id ${req.params.id}`
+        );
+    } else {
+      res.send("Deleted with success");
+    }
+  });
 });
 
 app.listen(port, () => {
